f8ae1140029d30dc6e89d21ba1278b8c5b09afaa
Russell Archer <russell.archer@mac.com>
2022-01-24 23:54:27 +0000

    Initial commit
    Signed-off-by: Russell Archer <russell.archer@mac.com>

---
+++ README.md
@@ -0,0 +1,12 @@
+#  StoreHelperDemo
+
+Implementing and testing in-app purchases with **SwiftUI**, `StoreHelper`, `StoreKit2`, **Xcode 13**, **iOS 15** and **macOS 12**.
+
+This demo app uses the See [StoreHelper](https://github.com/russell-archer/IAPDemo) package to simplify working with in-app purchases in Xcode 13.
+
+> This demo app requires `StoreKit2`, `StoreHelper`, Swift 5.5, Xcode 13 and iOS 15. 
+> 
+> See [StoreHelper](https://github.com/russell-archer/IAPDemo) for background details on working with `StoreKit2` and `StoreHelper`.
+> See [In-App Purchases with Xcode 12 and iOS 14](https://github.com/russell-archer/IAPDemo) for details of working with the original `StoreKit1` in iOS 14 and lower.
+
+
--- Shared/StoreHelperDemoApp.swift
+++ Shared/StoreHelperDemoApp.swift
@@ -6,12 +6,24 @@
 //
 
 import SwiftUI
+import StoreHelper
 
 @main
 struct StoreHelperDemoApp: App {
+    @StateObject var storeHelper = StoreHelper()
+    
+    #if os(macOS)
+    let minScreenSize = CGSize(width: 600, height: 600)
+    let defaultScreenSize = CGSize(width: 700, height: 700)
+    #endif
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView().environmentObject(storeHelper)
+                #if os(macOS)
+                .frame(minWidth: minScreenSize.width, idealWidth: defaultScreenSize.width, minHeight: minScreenSize.height, idealHeight: defaultScreenSize.height)
+                .font(.title2)  // Default font
+                #endif
         }
     }
 }
