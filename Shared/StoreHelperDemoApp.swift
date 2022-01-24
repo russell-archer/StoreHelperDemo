59788020177c46dc10f6043d1b5cf418f111d883
Russell Archer <russell.archer@mac.com>
2022-01-24 23:22:32 +0000

    Initial Commit

---
+++ Shared/StoreHelperDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreHelperDemoApp.swift
+//  Shared
+//
+//  Created by Russell Archer on 24/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct StoreHelperDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
