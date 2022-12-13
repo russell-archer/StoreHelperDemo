b3db2cd06f0b34b1c5592c328496e2cc64b3c556
Russell Archer <russell.archer@mac.com>
2022-12-13 14:00:31 +0000

    Removed ScrollView in ContentView as this is now handled in Products

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -17,24 +17,23 @@ import StoreHelper
 ///
 @available(iOS 15.0, macOS 12.0, *)
 struct ContentView: View {
+    @EnvironmentObject var storeHelper: StoreHelper
     @State private var showProductInfoSheet = false
     @State private var productId: ProductId = ""
     
     var body: some View {
-        ScrollView {
-            Products() { id in
-                productId = id
-                showProductInfoSheet = true
+        Products() { id in
+            productId = id
+            showProductInfoSheet = true
+        }
+        .sheet(isPresented: $showProductInfoSheet) {
+            VStack {
+                // Pull in text and images that explain the particular product identified by `productId`
+                ProductInfo(productInfoProductId: $productId, showProductInfoSheet: $showProductInfoSheet)
             }
-            .sheet(isPresented: $showProductInfoSheet) {
-                VStack {
-                    // Pull in text and images that explain the particular product identified by `productId`
-                    ProductInfo(productInfoProductId: $productId, showProductInfoSheet: $showProductInfoSheet)
-                }
-                #if os(macOS)
-                .frame(minWidth: 500, idealWidth: 500, maxWidth: 500, minHeight: 500, idealHeight: 500, maxHeight: 500)
-                #endif
-            }
+            #if os(macOS)
+            .frame(minWidth: 500, idealWidth: 500, maxWidth: 500, minHeight: 500, idealHeight: 500, maxHeight: 500)
+            #endif
         }
     }
 }
