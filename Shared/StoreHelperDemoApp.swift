8ca8a51afdbb85e073fa05904fc7f4f512531253
Russell Archer <russell.archer@mac.com>
2024-01-24 12:20:38 +0000

    Turned off verbose logging
    Signed-off-by: Russell Archer <russell.archer@mac.com>

--- Shared/StoreHelperDemoApp.swift
+++ Shared/StoreHelperDemoApp.swift
@@ -17,7 +17,10 @@ struct StoreHelperDemoApp: App {
         WindowGroup {
             MainView()
                 .environmentObject(storeHelper)
-                .task { storeHelper.start() }  // Start listening for transactions
+                .task {
+                    storeHelper.start()  // Start listening for transactions
+                    StoreLog.logIsPurchasedEvents = false  // Set to true for verbose logging
+                }
                 #if os(macOS)
                 .frame(minWidth: 700, idealWidth: 700, minHeight: 700, idealHeight: 700)
                 .font(.title2)
